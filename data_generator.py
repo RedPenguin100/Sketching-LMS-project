d3c52914d2394a8015edfee7db46d2c92943fdf0
Michael Kovaliov <michaelkovaliov97@gmail.com>
2020-04-22 21:20:37 +0300

    refactor

--- data_generator.py
+++ data_generator.py
@@ -5,10 +5,9 @@ def get_dummy_data():
     n = 1000000
     A = np.empty((n, 2))
     b = np.arange(n)
-    A[:, 0] = b
-    A[:, 1] = 1
     rand = np.random.uniform(size=2 * n)
+    A[:, 0] = b + rand[n:]
+    A[:, 1] = 1
     b = b + rand[0:n]
-    A[:, 0] = A[:, 0] + rand[n:]
     b.resize(n, 1)
     return A, b
